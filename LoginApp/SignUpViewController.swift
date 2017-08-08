bb64b20eb8bc7c83dcbbe96813b1dcc1c1b4d317
Danny Franklin <dannyfranklin@dannys-air.fios-router.home>
2017-08-07 23:20:22 -0400

    Add in a sign up page
    Also navigation to that page

---
+++ LoginApp/SignUpViewController.swift
@@ -0,0 +1,69 @@
+//
+//  SignUpViewController.swift
+//  UnBlock
+//
+//  Created by Danny Franklin on 8/7/17.
+//  Copyright © 2017 VDN. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    static let MIN_PASSWORD_LENGTH = 8
+    
+    @IBOutlet weak var passwordConfirm: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var username: UITextField!
+
+    @IBAction func signUp(_ sender: UIButton) {
+        guard password.text == passwordConfirm.text else {
+            let alert = UIAlertController(title: "Password doesn't match", message: "Please try again", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        guard password.text!.characters.count >= SignUpViewController.MIN_PASSWORD_LENGTH else {
+            let alert = UIAlertController(title: "Password must be at least 8 characters", message: "Please try again", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        let apiManager = UnblockApiManager.sharedInstance
+        apiManager.signUp(username:username.text!, email: email.text!, password:password.text!, completion:handleSignUp)
+    }
+    
+    func handleSignUp(_ success: Bool, token: String?) {
+        let keychain = KeychainSwift()
+        guard success && keychain.set(token!, forKey: LoginViewController.LOGIN_TOKEN) else {
+            DispatchQueue.main.async(execute: {
+                let alert = UIAlertController(title: "Login failed", message: "Please try again", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            })
+            return
+        }
+        
+        goToLoggedIn()
+    }
+    
+    func goToLoggedIn() {
+        DispatchQueue.main.async(execute: {
+            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let loginSuccessViewController = mainStoryboard.instantiateViewController(withIdentifier:"LoginSuccessViewController")
+            self.present(loginSuccessViewController, animated: true, completion: nil)
+        })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- LoginApp/UnbockApiManager.swift
+++ LoginApp/UnbockApiManager.swift
@@ -61,6 +61,66 @@ class UnblockApiManager: NSObject {
             completion(true, token)
         }
         task.resume()
+    }
+    
+    func signUp(username: String, email: String, password: String, completion: @escaping(_ loginResponse: Bool,_ token: String?) -> Void) {
+        guard let loginURL = URL(string: baseURL + "newUser") else {
+            print("Error: cannot create URL")
+            completion(false, nil)
+            return
+        }
+        
+        var loginUrlRequest = URLRequest(url: loginURL)
+        loginUrlRequest.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")  // the request is JSON
+        
+        loginUrlRequest.httpMethod = "POST"
+        let newLogin: [String: Any] = [
+            "username": username,
+            "password": password,
+            "email": email
+        ]
+        
+        let jsonLogin: Data
+        do {
+            jsonLogin = try JSONSerialization.data(withJSONObject: newLogin, options: [])
+            loginUrlRequest.httpBody = jsonLogin
+        } catch {
+            print("Error: cannot create JSON from login")
+            completion(false, nil)
+            return
+        }
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: loginUrlRequest) {
+            (data, response, error) in
+            print(response!.description)
+            print(data!.description)
+            guard error == nil else {
+                print(error!.localizedDescription)
+                completion(false, nil)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(false, nil)
+                return
+            }
+            
+            guard httpResponse.statusCode == 200 else {
+                completion(false, nil)
+                return
+            }
+            
+            guard let token = httpResponse.allHeaderFields["Authorization"] as! String? else {
+                completion(false, nil)
+                return
+            }
+            
+            completion(true, token)
+        }
+        task.resume()
         
     }
+
 }
